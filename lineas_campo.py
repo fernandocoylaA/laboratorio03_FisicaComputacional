38cfacb1e93809e705d6a167bfbfa85eb44686c8
fernandocoylaA <fcoylaa@unsa.edu.pe>
2024-05-14 11:29:48 -0500

    implementacion completa

---
+++ lineas_campo.py
@@ -0,0 +1,43 @@
+#Programa: Observar lineas de campo electrico
+
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+#Retorna el vector de campo eléctrico E=(Ex,Ey) de una carga q en r0
+def E(q, r0, x, y):    
+    den = np.hypot(x-r0[0], y-r0[1])**3
+    return q * (x - r0[0]) / den, q * (y - r0[1]) / den
+
+nx, ny = 64, 64
+x = np.linspace(-2, 2, nx)
+y = np.linspace(-2, 2, ny)
+X, Y = np.meshgrid(x, y) #malla bidimensional
+
+# Define las cargas con valores positivos y negativos
+charges = [(1, (-1, 0)), (1, (1, 0))]
+
+Ex, Ey = np.zeros((ny, nx)), np.zeros((ny, nx))
+for charge in charges:
+    ex, ey = E(*charge, x=X, y=Y)
+    Ex += ex
+    Ey += ey
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+color = 2 * np.log(np.hypot(Ex, Ey))
+ax.streamplot(x, y, Ex, Ey, color=color, linewidth=1, cmap=plt.cm.inferno,
+              density=2, arrowstyle='->', arrowsize=1.5)
+
+charge_colors = {True: '#aa0000', False: '#0000aa'}
+for q, pos in charges:
+    ax.add_artist(Circle(pos, 0.05, color=charge_colors[q>0]))
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$y$')
+ax.set_xlim(-2,2)
+ax.set_ylim(-2,2)
+ax.set_aspect('equal')
+plt.show()
